ffc875938015686c78abb173e180580d91eebf26
Pushpanjali Sonawane <pushpanjali.sonawane@uk.zurich.com>
2021-03-18 16:31:11 +0000

    Start Integrating With Trello

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask.globals import session
 from todo_app.data.session_items import add_item, get_items
 from todo_app.flask_config import Config
+from todo_app.trello_items import Get_items_from_trello, create_card,move_card
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -9,13 +11,19 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index(): 
-    items = get_items()
+    items = Get_items_from_trello()
     return render_template("index.html", items=items)
     
 @app.route('/', methods=['POST'])
 def add_todo(): 
     item = request.form.get('additem')
-    add_item(item)
+    create_card(item)
+    return redirect('/')
+
+@app.route('/move', methods=['POST'])
+def Move_InTrello(): 
+    item = request.form.get('id')
+    move_card(item)
     return redirect('/')
     
 
