a8dfc10eaf9e6b5f2d9f3cbcaaeae52311a9a2d6
Pushpanjali Sonawane <pushpanjali.sonawane@uk.zurich.com>
2021-05-07 14:46:28 +0100

    Changes in Item class

--- todo_app/Item.py
+++ todo_app/Item.py
@@ -1,8 +1,8 @@
 class Item:
     def __init__(self,id,name,status,idList):
-        self.id=id,
-        self.name=name,
-        self.status=status,
+        self.id=id
+        self.name=name
+        self.status=status
         self.idList=idList
 
 
